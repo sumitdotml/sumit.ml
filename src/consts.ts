adc34e2a27029c72ddaed565bed8a6942e268406
sumit <sumit@sumit.ml>
2026-01-30 05:36:39 +0900

    centralize worker url in consts and add research description

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,8 @@ export const SITE_DESCRIPTION =
 	"a site for my ml r&d worklogs and nonsensical thoughts";
 
 export const BLOG_BASE_PAGE_DESCRIPTION = "my ml worklogs and blog dumps";
+
+export const RESEARCH_BASE_PAGE_DESCRIPTION =
+	"technical reports and research writeups";
+
+export const WORKER_URL = import.meta.env.PUBLIC_WORKER_URL ?? "";
